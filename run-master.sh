6e20ae979b141c7d0a567ad7ca9e0b479a890009
root <root@instance-1.us-central1-a.c.sturdy-coast-284109.internal>
2020-10-28 09:20:18 +0000

    instance-1,2,3

--- run-master.sh
+++ run-master.sh
@@ -1,9 +1,10 @@
 apt install -y git
 curl https://get.docker.com | sudo bash
 
-mkdir -p /srv/docker
+mkdir -p /srv/docker 
+mkdir -p /srv/nfs
 #	Configurar NFS
-echo 'maquina-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
+echo 'instance-1:/srv/nfs /srv/docker nfs defaults,nfsvers=3 0 0' >> /etc/fstab
 apt install -y nfs-kernel-server
 echo '/srv/nfs 10.132.0.0/24(rw,no_root_squash,no_subtree_check)' >> /etc/exports
 systemctl start nfs-kernel-server
